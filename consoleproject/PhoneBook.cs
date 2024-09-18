2c4f84f7629701ceb155615e9876c5d4bceee592
tshepononyane <nonyanetshepo8@gmail.com>
2024-09-18 20:07:04 +0200

    Add project files.

---
+++ consoleproject/PhoneBook.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleproject
+{
+    internal class PhoneBook
+    {
+        private List<Contact> _contacts {get; set; } = new List<Contact>();
+
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+
+        }
+
+        private void DisplayContactsDetails(List<Contact> contacts) 
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+
+            }
+
+
+        }
+        public void AddContact(Contact contact) 
+        { 
+            _contacts.Add(contact);
+        
+        }
+
+        public void DisplayContant(string number) 
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+
+            }
+            else 
+            {
+               DisplayContactDetails (contact );
+            
+            }
+        
+        }
+
+        public void DisplayAllContact() 
+        {
+            DisplayContactsDetails(_contacts);
+        
+        }
+
+        public void DisplayMatchingContacts(string searchPhrase) 
+        { 
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContactsDetails(matchingContacts);
+        }
+    }
+}
---
+++ consoleproject/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\t\t\t\t\t\t\t~~~PHONE BOOK APP~~~");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n CHOOSE A NUMBER\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("_______________________________________");
+            Console.WriteLine("1 ADD CONTACT");
+            Console.WriteLine("2 SEARCH FOR A  CONTACT BY NUMBER");
+            Console.WriteLine("3 VIEW ALL CONTACTS");
+            Console.WriteLine("4 SEARCH FOR CONTACTS FOR A BY NAME");
+            Console.WriteLine("PRESS 'x' TO EXIT");
+            Console.WriteLine("_______________________________________");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var userinput = Console.ReadLine();
+            var phoneBook = new PhoneBook();
+            
+            while (true)
+            {
+                switch (userinput)
+                {
+                    case "1":
+                        Console.WriteLine("CONTACT NAME");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("CONTACT NUMBER");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name,number);
+                        phoneBook.AddContact(newContact);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("CONTACT NUMBER TO SEARCH");
+                        var searchNumber = Console.ReadLine();
+                        phoneBook.DisplayContant(searchNumber);
+                        break;
+                   case "3":
+                        phoneBook.DisplayAllContact();
+                        break;
+                    case "4":
+                        Console.WriteLine("Name Search Phrase");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+                    
+                    case "x":
+                        return;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("WRONG INPUT");
+                        break;
+
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("CHOOSE A NUMBER");
+                userinput = Console.ReadLine();
+
+
+            }
+
+
+
+
+        }
+    }
+}
